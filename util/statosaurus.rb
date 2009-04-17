bba9006df84d81862baad65317338ba185f7e556
Nick Kallen <nkallen@yahoo.com>
2009-04-17 11:21:54 -0700

    thread safe stats

--- util/statosaurus.rb
+++ util/statosaurus.rb
@@ -1,11 +1,17 @@
 class Statosaurus
-  attr_accessor :transaction_id
-
   def initialize(fields, logger)
     @fields = fields.sort
-    @values = {}
+    values = {}
     @logger = logger
     @logger.info("# Fields: " + fields.join(" "))
+  end
+  
+  def values
+    Thread.current[:values] ||= {}
+  end
+  
+  def transaction_id
+    Thread.current[:transaction_id]
   end
 
   def measure(field, &block)
@@ -13,28 +19,29 @@ class Statosaurus
     measurement = Benchmark.measure do
       result = yield
     end
-    @values["#{field}_real"] = min(measurement.real)
-    @values["#{field}_sys"] = min(measurement.stime)
-    @values["#{field}_user"] = min(measurement.utime)
+    values["#{field}_real"] = min(measurement.real)
+    values["#{field}_sys"] = min(measurement.stime)
+    values["#{field}_user"] = min(measurement.utime)
     result
   end
   
   def set(key, value)
-    @values[key] = value
+    values[key] = value
   end
 
   def transaction
-    @transaction_id = "#{Process.pid}-#{Time.now.to_i}-#{rand(9999)}"
+    Thread.current[:transaction_id] = "#{Process.pid}-#{Time.now.to_i}-#{rand(9999)}"
     result = yield
     print
-    @values, @transaction_id = {}, nil
+    values.clear
+    Thread.current[:transaction_id]= nil
     result
   end
   
   private
   def print
-    prefix = [Time.now.iso8601, @transaction_id].join(" ")
-    info = @fields.collect { |field| format(@values[field]) }.join(" ")
+    prefix = [Time.now.iso8601, transaction_id].join(" ")
+    info = @fields.collect { |field| format(values[field]) }.join(" ")
     @logger.info(prefix + " " + info)
   end
   
